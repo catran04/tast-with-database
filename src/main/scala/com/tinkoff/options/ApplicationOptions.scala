9ef640c6088710e46b0e5a97f36f121b6dbe1b02
aandrosov <coffee-04@yandex.ru>
2018-03-06 20:46:14 +0300

    updated ApplicationOptions

--- src/main/scala/com/tinkoff/options/ApplicationOptions.scala
+++ src/main/scala/com/tinkoff/options/ApplicationOptions.scala
@@ -8,9 +8,30 @@ case class ApplicationOptions(
                              mysql: MysqlOptions = MysqlOptions(),
                              redis: RedisOptions = RedisOptions()
                              ) {
-
 }
 
+object ApplicationOptions {
+  val defaults = ApplicationOptions(Array.empty[String])
+
+  /**
+    * Initialize options with given arguments
+    */
+  def apply(args: Array[String]): ApplicationOptions = {
+    if(args.isEmpty) return ApplicationOptions.defaults
+    args.foldLeft(ApplicationOptions()) { (options, arg) =>
+      arg.split("=") match {
+        case Array("rest.host", value) => options.copy(rest = options.rest.copy(host = value))
+        case Array("rest.port", value) => options.copy(rest = options.rest.copy(port = value.toInt))
+        case Array("mysql.host", value) => options.copy(mysql = options.mysql.copy(host = value))
+        case Array("mysql.port", value) => options.copy(mysql = options.mysql.copy(port = value.toInt))
+        case Array("redis.host", value) => options.copy(redis = options.redis.copy(host = value))
+        case Array("redis.port", value) => options.copy(redis = options.redis.copy(port = value.toInt))
+        case Array("redis.delimiter", value) => options.copy(redis = options.redis.copy(delimiter = value))
+        case exc => throw new RuntimeException(s"invalid args: ${exc.mkString}")
+      }
+    }
+  }
+}
 case class RestOptions(
                       host: String = "localhost",
                       port: Int = 9080
@@ -22,7 +43,7 @@ case class MysqlOptions(
                        ){}
 
 case class RedisOptions (
-                          val host: String = "localhost",
+                          host: String = "localhost",
                           port: Int = 6379,
                           delimiter: String = "."
                         ){}
